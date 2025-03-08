mjohndus/adminer

Refactor core classes
- Different class names for Adminer and Editor.
- AdminerPlugin renamed to Pluginer.
- Adminer, Editor and Pluginer implements the same AdminerInterface.
- Remove AdminerDesigns plugin.

adminer/plugins.php
@@ -1,0 +1,54 @@
+<?php
+
+use Adminer\AdminerDatabaseHide;
+use Adminer\AdminerDumpAlter;
+use Adminer\AdminerDumpBz2;
+use Adminer\AdminerDumpJson;
+use Adminer\AdminerDumpXml;
+use Adminer\AdminerDumpZip;
+use Adminer\AdminerEditForeign;
+use Adminer\AdminerEnumOption;
+use Adminer\AdminerFileUpload;
+use Adminer\AdminerForeignSystem;
+use Adminer\AdminerInterface;
+use Adminer\AdminerJsonColumn;
+use Adminer\AdminerLoginSsl;
+use Adminer\AdminerSlugify;
+use Adminer\AdminerTranslation;
+use Adminer\Pluginer;
+
+function create_adminer(): AdminerInterface
+{
+	foreach (glob("../plugins/*.php") as $filename) {
+		include $filename;
+	}
+
+	$plugins = [
+		new AdminerDatabaseHide(['information_schema']),
+		new AdminerDumpJson,
+		new AdminerDumpBz2,
+		new AdminerDumpZip,
+		new AdminerDumpXml,
+		new AdminerDumpAlter,
+		// new AdminerSqlLog("past-" . rtrim(`git describe --tags --abbrev=0`) . ".sql"),
+		// new AdminerEditCalendar(script_src("../externals/jquery-ui/jquery-1.4.4.js") . script_src("../externals/jquery-ui/ui/jquery.ui.core.js") . script_src("../externals/jquery-ui/ui/jquery.ui.widget.js") . script_src("../externals/jquery-ui/ui/jquery.ui.datepicker.js") . script_src("../externals/jquery-ui/ui/jquery.ui.mouse.js") . script_src("../externals/jquery-ui/ui/jquery.ui.slider.js") . script_src("../externals/jquery-timepicker/jquery-ui-timepicker-addon.js") . "<link rel='stylesheet' href='../externals/jquery-ui/themes/base/jquery.ui.all.css'>\n<style>\n.ui-timepicker-div .ui-widget-header { margin-bottom: 8px; }\n.ui-timepicker-div dl { text-align: left; }\n.ui-timepicker-div dl dt { height: 25px; }\n.ui-timepicker-div dl dd { margin: -25px 0 10px 65px; }\n.ui-timepicker-div td { font-size: 90%; }\n</style>\n", "../externals/jquery-ui/ui/i18n/jquery.ui.datepicker-%s.js"),
+		// new AdminerTinymce("../externals/tinymce/jscripts/tiny_mce/tiny_mce_dev.js"),
+		// new AdminerWymeditor(array("../externals/wymeditor/src/jquery/jquery.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.explorer.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.mozilla.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.opera.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.safari.js")),
+		new AdminerFileUpload(""),
+		new AdminerJsonColumn,
+		new AdminerSlugify,
+		new AdminerTranslation,
+		new AdminerForeignSystem,
+		new AdminerEnumOption,
+		new AdminerEditForeign,
+		new AdminerLoginSsl(["TrustServerCertificate" => true]),
+	];
+
+	$config = [
+		"theme" => "default-green",
+	];
+
+	return new Pluginer($plugins, $config);
+}
+
+include "index.php";

editor/example.php
@@ -2,54 +2,65 @@
 
 namespace Adminer;
 
-function adminer_object() {
-
-	class AdminerCds extends Adminer {
-
-		function name() {
+function create_adminer(): AdminerInterface
+{
+	class CdsEditor extends Editor
+	{
+		function name()
+		{
 			// custom name in title and heading
 			return 'CDs';
 		}
 
-		function credentials() {
+		function credentials()
+		{
 			// ODBC user without password on localhost
 			return array('localhost', 'ODBC', '');
 		}
 
-		function database() {
+		function database()
+		{
 			// will be escaped by Adminer
 			return 'adminer_test';
 		}
 
-		function login($login, $password) {
+		function login($login, $password)
+		{
 			// username: 'admin', password: anything
 			return ($login == 'admin');
 		}
 
-		function tableName($tableStatus) {
+		function tableName($tableStatus)
+		{
 			// tables without comments would return empty string and will be ignored by Adminer
 			return h($tableStatus["Comment"]);
 		}
 
-		function fieldName($field, $order = 0) {
+		function fieldName($field, $order = 0)
+		{
 			if ($order && preg_match('~_(md5|sha1)$~', $field["field"])) {
 				return ""; // hide hashes in select
 			}
+
 			// display only column with comments, first five of them plus searched columns
 			if ($order < 5) {
 				return h($field["comment"]);
 			}
-			foreach ((array) $_GET["where"] as $key => $where) {
+
+			foreach ((array)$_GET["where"] as $key => $where) {
 				if ($where["col"] == $field["field"] && ($key >= 0 || $where["val"] != "")) {
 					return h($field["comment"]);
 				}
 			}
+
 			return "";
 		}
 
 	}
 
-	return new AdminerCds;
+	return new CdsEditor([
+		"theme" => "default-green",
+	]);
 }
 
-include "./index.php";
+include "index.php";

editor/index.php
@@ -13,7 +13,12 @@
 
 namespace Adminer;
 
+include "../adminer/core/AdminerInterface.php";
+include "core/Editor.php";
+
 include "../adminer/include/bootstrap.inc.php";
+include "include/connect.inc.php";
+
 $drivers[DRIVER] = lang('Login');
 
 if (isset($_GET["select"]) && ($_POST["edit"] || $_POST["clone"]) && !$_POST["save"]) {
@@ -27,10 +32,15 @@ if (isset($_GET["download"])) {
 } elseif (isset($_GET["select"])) {
 	include "../adminer/select.inc.php";
 } elseif (isset($_GET["script"])) {
-	include "./script.inc.php";
+	include "script.inc.php";
 } else {
-	include "./db.inc.php";
+	include "db.inc.php";
 }
 
 // each page calls its own page_header(), if the footer should not be called then the page exits
 page_footer();
+
+function create_instance(array $config = []): AdminerInterface
+{
+	return new Editor($config);
+}

adminerneo/adminerneo

adminer/plugins.php
@@ -1,0 +1,53 @@
+<?php
+
+use Adminer\AdminerDatabaseHide;
+use Adminer\AdminerDumpAlter;
+use Adminer\AdminerDumpBz2;
+use Adminer\AdminerDumpJson;
+use Adminer\AdminerDumpXml;
+use Adminer\AdminerDumpZip;
+use Adminer\AdminerEditForeign;
+use Adminer\AdminerEnumOption;
+use Adminer\AdminerFileUpload;
+use Adminer\AdminerForeignSystem;
+use Adminer\AdminerJsonColumn;
+use Adminer\AdminerLoginSsl;
+use Adminer\AdminerSlugify;
+use Adminer\AdminerTranslation;
+use Adminer\Pluginer;
+
+function create_adminer(): Pluginer
+{
+	foreach (glob("../plugins/*.php") as $filename) {
+		include $filename;
+	}
+
+	$plugins = [
+		new AdminerDatabaseHide(['information_schema']),
+		new AdminerDumpJson,
+		new AdminerDumpBz2,
+		new AdminerDumpZip,
+		new AdminerDumpXml,
+		new AdminerDumpAlter,
+		// new AdminerSqlLog("past-" . rtrim(`git describe --tags --abbrev=0`) . ".sql"),
+		// new AdminerEditCalendar(script_src("../externals/jquery-ui/jquery-1.4.4.js") . script_src("../externals/jquery-ui/ui/jquery.ui.core.js") . script_src("../externals/jquery-ui/ui/jquery.ui.widget.js") . script_src("../externals/jquery-ui/ui/jquery.ui.datepicker.js") . script_src("../externals/jquery-ui/ui/jquery.ui.mouse.js") . script_src("../externals/jquery-ui/ui/jquery.ui.slider.js") . script_src("../externals/jquery-timepicker/jquery-ui-timepicker-addon.js") . "<link rel='stylesheet' href='../externals/jquery-ui/themes/base/jquery.ui.all.css'>\n<style>\n.ui-timepicker-div .ui-widget-header { margin-bottom: 8px; }\n.ui-timepicker-div dl { text-align: left; }\n.ui-timepicker-div dl dt { height: 25px; }\n.ui-timepicker-div dl dd { margin: -25px 0 10px 65px; }\n.ui-timepicker-div td { font-size: 90%; }\n</style>\n", "../externals/jquery-ui/ui/i18n/jquery.ui.datepicker-%s.js"),
+		// new AdminerTinymce("../externals/tinymce/jscripts/tiny_mce/tiny_mce_dev.js"),
+		// new AdminerWymeditor(array("../externals/wymeditor/src/jquery/jquery.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.explorer.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.mozilla.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.opera.js", "../externals/wymeditor/src/wymeditor/jquery.wymeditor.safari.js")),
+		new AdminerFileUpload(""),
+		new AdminerJsonColumn,
+		new AdminerSlugify,
+		new AdminerTranslation,
+		new AdminerForeignSystem,
+		new AdminerEnumOption,
+		new AdminerEditForeign,
+		new AdminerLoginSsl(["TrustServerCertificate" => true]),
+	];
+
+	$config = [
+		"theme" => "default-green",
+	];
+
+	return new Pluginer($plugins, $config);
+}
+
+include "index.php";

editor/example.php
@@ -3,54 +3,65 @@
 use Adminer\Adminer;
 use function Adminer\h;
 
-function adminer_object() {
-
-	class AdminerCds extends Adminer {
-
-		function name() {
+function create_adminer(): Adminer
+{
+	class CdsEditor extends Adminer
+	{
+		function name()
+		{
 			// custom name in title and heading
 			return 'CDs';
 		}
 
-		function credentials() {
+		function credentials()
+		{
 			// ODBC user with password ODBC on localhost
 			return array('localhost', 'ODBC', 'ODBC');
 		}
 
-		function database() {
+		function database()
+		{
 			// will be escaped by Adminer
 			return 'adminer_test';
 		}
 
-		function login($login, $password) {
+		function login($login, $password)
+		{
 			// username: 'admin', password: anything
 			return ($login == 'admin');
 		}
 
-		function tableName($tableStatus) {
+		function tableName($tableStatus)
+		{
 			// tables without comments would return empty string and will be ignored by Adminer
 			return h($tableStatus["Comment"]);
 		}
 
-		function fieldName($field, $order = 0) {
+		function fieldName($field, $order = 0)
+		{
 			if ($order && preg_match('~_(md5|sha1)$~', $field["field"])) {
 				return ""; // hide hashes in select
 			}
+
 			// display only column with comments, first five of them plus searched columns
 			if ($order < 5) {
 				return h($field["comment"]);
 			}
-			foreach ((array) $_GET["where"] as $key => $where) {
+
+			foreach ((array)$_GET["where"] as $key => $where) {
 				if ($where["col"] == $field["field"] && ($key >= 0 || $where["val"] != "")) {
 					return h($field["comment"]);
 				}
 			}
+
 			return "";
 		}
 
 	}
 
-	return new AdminerCds;
+	return new CdsEditor([
+		"theme" => "default-green",
+	]);
 }
 
-include "./index.php";
+include "index.php";

editor/index.php
@@ -16,7 +16,12 @@
 
 namespace Adminer;
 
+include "../adminer/core/AdminerBase.php";
+include "core/Adminer.php";
+
 include "../adminer/include/bootstrap.inc.php";
+include "include/connect.inc.php";
+
 $drivers[DRIVER] = lang('Login');
 
 if (isset($_GET["select"]) && ($_POST["edit"] || $_POST["clone"]) && !$_POST["save"]) {
@@ -30,9 +35,9 @@ if (isset($_GET["download"])) {
 } elseif (isset($_GET["select"])) {
 	include "../adminer/select.inc.php";
 } elseif (isset($_GET["script"])) {
-	include "./script.inc.php";
+	include "script.inc.php";
 } else {
-	include "./db.inc.php";
+	include "db.inc.php";
 }
 
 // each page calls its own page_header(), if the footer should not be called then the page exits